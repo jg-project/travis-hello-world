a8121e5df4fa11078fa123efd6f9d57558302bc2
vdyptan <Sanctorum@ukr.net>
2016-02-09 22:12:47 +0200

    commit #4

--- src/test/groovy/jgproject/travis/lecture_two/SorterImplTest.groovy
+++ src/test/groovy/jgproject/travis/lecture_two/SorterImplTest.groovy
@@ -1,90 +1,89 @@
+/*
 package jgproject.travis.lecture_two
+
 
 class SorterImplTest extends GroovyTestCase {
 
-    Sorter sorter;
+   Sorter sorter;
 
-    static Random rnd = new Random();
+   static Random rnd = new Random();
 
-    static int[] generateArray(int size) {
-        int[] result
-        if (size > -1) {
-            result = new int[size]
-            size.times { index ->
-                result[index] = rnd.nextInt();
-            }
-        }
-        result
-    }
+   static int[] generateArray(int size) {
+       int[] result
+       if (size > -1) {
+           result = new int[size]
+           size.times { index ->
+               result[index] = rnd.nextInt();
+           }
+       }
+       result
+   }
 
-    void setUp() {
-        sorter = new SorterImpl();
-    }
+   void setUp() {
+       sorter = new SorterImpl();
+   }
 
-    void testSortsEmptyArray() {
-        int[] arr = new int[0]
-        int[] expectedArr = new int[0]
+   void testSortsEmptyArray() {
+       int[] arr = new int[0]
+       int[] expectedArr = new int[0]
 
-        sorter.sort(arr)
+       sorter.sort(arr)
 
-        assertArraysSame(expectedArr, arr)
-    }
+       assertArraysSame(expectedArr, arr)
+   }
 
-   /* void testSortsNonEmptyArrays() {
-        (1..1000).each {
-            int[] arr = generateArray(it)
-            int[] expectedArr = new int[it]
-            System.arraycopy(arr, 0, expectedArr, 0, it)
-            Arrays.sort(expectedArr)
+   void testSortsNonEmptyArrays() {
+       (1..1000).each {
+           int[] arr = generateArray(it)
+           int[] expectedArr = new int[it]
+           System.arraycopy(arr, 0, expectedArr, 0, it)
+           Arrays.sort(expectedArr)
 
-            sorter.sort(arr)
-            assertArraysSame(expectedArr, arr)
-        }
-    }
-   */
+           sorter.sort(arr)
+           assertArraysSame(expectedArr, arr)
+       }
+   }
 
-    static assertArraysSame(int[] a, int[] b) {
-        assert a != null && b != null && a.length == b.length
+   static assertArraysSame(int[] a, int[] b) {
+       assert a != null && b != null && a.length == b.length
 
-        for (int i = 0; i < a.length; i++) {
-            assert a[i] == b[i]
-        }
-    }
+       for (int i = 0; i < a.length; i++) {
+           assert a[i] == b[i]
+       }
+   }
 
-    static assertListsSame(Collection a, Collection b) {
-        assert a != null && b != null && a.size() == b.size()
+   static assertListsSame(Collection a, Collection b) {
+       assert a != null && b != null && a.size() == b.size()
 
-        def aIt = a.iterator()
-        def bIt = b.iterator()
+       def aIt = a.iterator()
+       def bIt = b.iterator()
 
-        while (aIt.hasNext() && bIt.hasNext()) {
-            assert aIt.next() == bIt.next()
-        }
-    }
+       while (aIt.hasNext() && bIt.hasNext()) {
+           assert aIt.next() == bIt.next()
+       }
+   }
 
-    void testSortsEmptyCollection() {
-        List<Integer> collection = new ArrayList<>()
-        List<Integer> expectedCollection = new ArrayList<>()
+   void testSortsEmptyCollection() {
+       List<Integer> collection = new ArrayList<>()
+       List<Integer> expectedCollection = new ArrayList<>()
 
-        sorter.sort(collection)
+       sorter.sort(collection)
 
-        assertListsSame(collection, expectedCollection)
-    }
+       assertListsSame(collection, expectedCollection)
+   }
 
- /*
-    void testSortsNonEmptyCollection() {
-        (1..1000).each {
-            List<Integer> collection = new ArrayList<>(Arrays.asList(generateArray(it)))
-            List<Integer> expectedCollection = new ArrayList<>(collection)
+   void testSortsNonEmptyCollection() {
+       (1..1000).each {
+           List<Integer> collection = new ArrayList<>(Arrays.asList(generateArray(it)))
+           List<Integer> expectedCollection = new ArrayList<>(collection)
 
-            Collections.sort(expectedCollection)
+           Collections.sort(expectedCollection)
 
-            sorter.sort(collection)
+           sorter.sort(collection)
 
-            assertListsSame(expectedCollection, collection)
-        }
-    }
-*/
+           assertListsSame(expectedCollection, collection)
+       }
+   }
 
     void testNullIsProcessedCorrectly() {
         int[] arr = null
@@ -95,3 +94,4 @@ class SorterImplTest extends GroovyTestCase {
 
 
 }
+*/
