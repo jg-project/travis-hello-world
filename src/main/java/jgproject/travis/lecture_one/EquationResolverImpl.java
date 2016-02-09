bb228e0709c1d82d8935549f0c23cd6e3783072d
Andrey Sevruk <sevrukandrey@gmail.com>
2016-02-09 17:01:29 +0200

    v1

--- src/main/java/jgproject/travis/lecture_one/EquationResolverImpl.java
+++ src/main/java/jgproject/travis/lecture_one/EquationResolverImpl.java
@@ -5,6 +5,23 @@ import jgproject.travis.lecture_one.dto.SquareRootResult;
 public class EquationResolverImpl implements EquationResolver {
 
     public SquareRootResult resolve(double a, double b, double c) {
-        return null;
+        double d = (b * b) - 4 * a * c;
+        double x1, x2;
+
+        if (d < 0) {
+            return null;
+        } else if (d >= 0) {
+            x1 = (((-1) * b - Math.sqrt(d))) / (2 * a);
+            x2 = (((-1) * b + Math.sqrt(d))) / (2 * a);
+            return new SquareRootResult(x1, x2);
+        } else {
+            x1 = (-1) * b / 2 * a;
+            x2 = x1;
+            return new SquareRootResult(x1, x2);
+        }
     }
 }
+
+
+
+
