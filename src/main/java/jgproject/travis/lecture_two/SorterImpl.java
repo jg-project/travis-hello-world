919719a0758460f7ab4df3038de84677e32e43bb
rfilipchak <rfilipchak@playtika.com.ua>
2016-02-26 11:06:45 +0200

    painkillers - changes1

--- src/main/java/jgproject/travis/lecture_two/SorterImpl.java
+++ src/main/java/jgproject/travis/lecture_two/SorterImpl.java
@@ -5,59 +5,59 @@ import java.util.List;
 public class SorterImpl implements Sorter {
     @Override
     public void sort(int... array) {
-//        int i;
-//        int temp;
+        int i;
+        int temp;
 
-//        for (i = array.length / 2 - 1; i >= 0; i--)
-//        {
-//            shiftDown(array, i, array.length);
-//        }
-//
-//        for (i = array.length - 1; i >= 1; i--)
-//        {
-//            temp = array[0];
-//            array[0] = array[i];
-//            array[i] = temp;
-//            shiftDown(array, 0, i);
-//        }
-//    }
-//    public void shiftDown(int[] a, int i, int j)
-//    {
-//        boolean done = false;
-//        int maxChild;
-//        int temp;
-//
-//        while ((i * 2 + 1 < j) && (!done))
-//        {
-//            if (i * 2 + 1 == j - 1)
-//                maxChild = i * 2 + 1;
-//            else if (a[i * 2 + 1] > a[i * 2 + 2])
-//                maxChild = i * 2 + 1;
-//            else
-//                maxChild = i * 2 + 2;
-//
-//            if (a[i] < a[maxChild])
-//            {
-//                temp = a[i];
-//                a[i] = a[maxChild];
-//                a[maxChild] = temp;
-//                i = maxChild;
-//            }
-//            else
-//            {
-//                done = true;
-//            }
-//
-//        }
-//
-//    }
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                swap(array, j , j +1 );
+        for (i = array.length / 2 - 1; i >= 0; i--)
+        {
+            shiftDown(array, i, array.length);
+        }
+
+        for (i = array.length - 1; i >= 1; i--)
+        {
+            temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            shiftDown(array, 0, i);
+        }
+    }
+    public void shiftDown(int[] a, int i, int j)
+    {
+        boolean done = false;
+        int maxChild;
+        int temp;
+
+        while ((i * 2 + 1 < j) && (!done))
+        {
+            if (i * 2 + 1 == j - 1)
+                maxChild = i * 2 + 1;
+            else if (a[i * 2 + 1] > a[i * 2 + 2])
+                maxChild = i * 2 + 1;
+            else
+                maxChild = i * 2 + 2;
+
+            if (a[i] < a[maxChild])
+            {
+                temp = a[i];
+                a[i] = a[maxChild];
+                a[maxChild] = temp;
+                i = maxChild;
             }
+            else
+            {
+                done = true;
+            }
+
         }
 
     }
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length - 1 - i; j++) {
+//                swap(array, j , j +1 );
+//            }
+//        }
+
+//   }
 
     public static void swap(int [] arr ,  int i, int j) {
 
