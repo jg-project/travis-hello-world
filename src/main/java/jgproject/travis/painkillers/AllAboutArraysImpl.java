3dd0df8019cc48ffd6002fe27d30955787bf0efb
tcherevyk <tcherevyk@playtika.com>
2016-03-01 14:54:45 +0200

    Painkillers

--- src/main/java/jgproject/travis/painkillers/AllAboutArraysImpl.java
+++ src/main/java/jgproject/travis/painkillers/AllAboutArraysImpl.java
@@ -1,5 +1,9 @@
 package jgproject.travis.painkillers;
 
+import jgproject.travis.lecture_two.Sorter;
+import jgproject.travis.lecture_two.SorterImpl;
+
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class AllAboutArraysImpl implements AllAboutArrays {
@@ -8,52 +12,131 @@ public class AllAboutArraysImpl implements AllAboutArrays {
 
     @Override
     public int[] createArray(int size) {
-        return new int[0];
+        return new int[size];
     }
 
     @Override
     public void fillWithRandomNumbers(int[] arr) {
+        ThreadLocalRandom randomElement = ThreadLocalRandom.current();
+        for (int i=0; i<arr.length; i++ ) {
+            arr[i] =  randomElement.nextInt();
+        }
     }
 
     @Override
     public void fillWithParticularNumber(int[] arr, int sample) {
+        Arrays.fill(arr, sample);
+    }
+
+    private int getSum(int[] arr) {
+        int sum = 0;
+        for (int i=0; i<arr.length; i++) {
+            sum = sum+arr[i];
+        }
+        return sum;
     }
 
     @Override
     public double getAggregate(int arr[], AggregationType aggregation) {
+        switch(aggregation) {
+            case CNT:
+               return arr.length;
+            case SUM:
+                return getSum(arr);
+            case AVG:
+                return (double)getSum(arr) / (double)arr.length;
+            case MIN:
+                sort(arr);
+                return arr[0];
+            case MAX:
+                sort(arr);
+                return arr[arr.length-1];
+            case MEDIAN:
+                if (arr.length % 2 == 0) {
+                    return (arr[arr.length/2-1] + arr[arr.length/2])/2;
+                }
+                else {
+                    return arr[arr.length/2];
+                }
+            case VARIANCE:
+                double sumArr = 0;
+                for (int i=0; i < arr.length; i++) {
+                    sumArr = Math.pow(arr[i] - (double)getSum(arr)/(double)arr.length, 2)+sumArr;
+                }
+                return sumArr/arr.length;
+        }
         return 0;
     }
 
     @Override
     public void copyArrays(int[] src, int pos, int[] dest, int startPos, int size) {
+        for (int i=0; i < size; i++) {
+            dest[startPos+i] = src[pos+i];
+        }
     }
 
     @Override
     public void reverseArray(int[] arr) {
+        int interim = 0;
+        for (int i=0; i<arr.length/2 +1; i++) {
+            interim = arr[i];
+            arr[i] = arr[arr.length - i-1];
+            arr[arr.length - i-1] = interim;
+        }
     }
 
     @Override
     public int[][] transpose(int[][] matrix) {
-        return new int[0][];
+        int[] innerArray = matrix[matrix.length-1];
+        int[][] transposedArr = new int[innerArray.length][matrix.length];
+        for (int i=0; i < innerArray.length; i++) {
+            for (int j=0; j < matrix.length; j++) {
+                transposedArr[i][j] = matrix [j][i];
+            }
+        }
+        return transposedArr;
     }
 
     @Override
     public int[] subarray(int[] src, int from, int to) {
-        return new int[0];
+        int[] subArray = new int[to-from+1];
+        for (int i=0; i< to-from+1; i++) {
+           subArray[i] = src[from+i-1];
+        }
+        return subArray;
     }
 
     @Override
     public void sort(int[] arr) {
-
+        Sorter sorter = new SorterImpl();
+        sorter.sort(arr);
     }
 
     @Override
     public int findEntryIndex(int[] arr, int whatToFind) {
-        return 0;
+        boolean flagEntry = false;
+        int i=0;
+        while (!flagEntry) {
+            if (arr[i] == whatToFind) {
+                flagEntry = true;
+            }
+            i++;
+        }
+        return i;
     }
 
     @Override
     public int[] removeDuplicates(int[] arr) {
-        return new int[0];
+        List<Integer> arrList = new ArrayList<Integer>();
+        for (int i : arr) {
+            if(!arrList.contains(i)) {
+                arrList.add(i);
+            }
+        }
+        int[] resultArr = new int[arrList.size()];
+        for (int i=0; i < arrList.size(); i++) {
+            resultArr[i] = arrList.get(i);
+        }
+        return resultArr;
     }
 }
--- src/test/groovy/jgproject/travis/painkillers/AllAboutArraysTest.groovy
+++ src/test/groovy/jgproject/travis/painkillers/AllAboutArraysTest.groovy
@@ -31,8 +31,6 @@ class AllAboutArraysTest extends GroovyTestCase {
             cnt += (runOne[i] == runTwo[i]) ? 0 : 1;
         }
         assert cnt > 1
-
-
     }
 
     void testFillWithParticularNumber() {
@@ -46,12 +44,12 @@ class AllAboutArraysTest extends GroovyTestCase {
 
     void testGetAggregate() {
         int[] arr = [1, 7, 11, 13, 501]
-        assert 5 == test.getAggregate(arr, CNT)
-        assert 1 == test.getAggregate(arr, MIN)
-        assert 501 == test.getAggregate(arr, MAX)
+        assert 5d == test.getAggregate(arr, CNT)
+        assert 1d == test.getAggregate(arr, MIN)
+        assert 501d == test.getAggregate(arr, MAX)
         assert Math.abs(106.6 - test.getAggregate(arr, AVG)) < 0.0000001
         assert Math.abs(533 - test.getAggregate(arr, SUM)) < 0.0000001
-        assert 11 == test.getAggregate(arr, MEDIAN)
+        assert 11d == test.getAggregate(arr, MEDIAN)
         assert Math.abs(38904.64 - test.getAggregate(arr, VARIANCE)) < 0.0000001
     }
 
@@ -59,32 +57,57 @@ class AllAboutArraysTest extends GroovyTestCase {
         int[] src = [1, 2, 3, 4, 500]
         int[] dest = new int[4]
         test.copyArrays(src, 2, dest, 0, 3)
-        assert [3,4,500,0] == dest
+        int[] expectedArr = [3,4,500,0]
+        assertArraysSame(dest, expectedArr)
     }
 
     void testReverseArray() {
         int[] arr = [1,100,15,100500,-30]
+        int[] reversedArr = [-30,100500,15,100,1]
         test.reverseArray(arr)
-        assert [-30,100500,15,100,1] == arr
+        assertArraysSame(arr,reversedArr)
     }
 
     void testTranspose() {
-
+        int[][] arr = [[1,2,3],[4,5,6]]
+        int[][] transposedArr = test.transpose(arr)
+        assert [[1,4],[2,5],[3,6]] == transposedArr
     }
 
     void testSubarray() {
-
+        int[] arr = [5,3,22,7,16,13]
+        int[] expectedArr = [22,7,16]
+        int[] subArray = test.subarray(arr,3,5)
+        assertArraysSame(expectedArr,subArray)
     }
 
     void testSort() {
-
+        int[] arr = [4,25,3,943,777]
+        int[] expectedArr = new int[arr.length]
+        System.arraycopy(arr, 0, expectedArr, 0,arr.length)
+        Arrays.sort(expectedArr)
+        test.sort(arr)
+        assert arr == expectedArr
     }
 
     void testFindEntryIndex() {
-
+        int[] arr = [84,285,6,6,1039,6]
+        int foundElementIndex = test.findEntryIndex(arr,6)
+        assert foundElementIndex == 3
     }
 
     void testRemoveDuplicates() {
+        int[] arr = [923,44,923,923,54,7,0,923]
+        int[] expectedArr = [923,44,54,7,0]
+        int[] uniqueElementsArr = test.removeDuplicates(arr)
+        assertArraysSame(expectedArr, uniqueElementsArr)
+    }
 
+    static assertArraysSame(int[] a, int[] b) {
+        assert a != null && b != null && a.length == b.length
+
+        for (int i = 0; i < a.length; i++) {
+            assert a[i] == b[i]
+        }
     }
 }
